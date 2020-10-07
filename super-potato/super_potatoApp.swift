ee4272a78d444944e2617fcb60d2303c03e63269
perekrist <perekrist31@gmail.com>
2020-10-07 19:39:14 +0700

    Initial Commit

---
+++ super-potato/super_potatoApp.swift
@@ -0,0 +1,17 @@
+//
+//  super_potatoApp.swift
+//  super-potato
+//
+//  Created by Кристина Перегудова on 07.10.2020.
+//
+
+import SwiftUI
+
+@main
+struct super_potatoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
